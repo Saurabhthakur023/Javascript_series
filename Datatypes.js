6faaa1fbbded334af700ca7a4d9aa9f0b447c222
Saurabhthakur023 <saurabhthakur02310@gmail.com>
2024-01-31 23:00:19 -0500

    Day 1

---
+++ Datatypes.js
@@ -0,0 +1,23 @@
+"use strict" // treat all my js code as newer version
+
+//alert(3+3) // we are using node.js not browser it can execute in browser
+
+let name="Saurabh "  //string datatype
+let age=18  //interger
+let isLogin=true  //boolean=> true /false
+
+// number range => 2 power 53
+// big integer
+// NULL => standalone value
+//undefined => value is assign
+//symbol=>check for unique
+
+//object
+
+
+console.log(typeof"Saurabh");
+console.log(typeof undefined); // undefined
+console.log(typeof null); //ouput is object
+
+
+
---
+++ varibale.js
@@ -0,0 +1,7 @@
+const accountId=14242
+let accountEmail="saurabhthakur02310@gmail.com"
+var accountPassword="12345"
+ let accountCity="Bareilly" 
+ let accountState;
+/* prefer not to use var because of functional scope */
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState]);
